f833be892de11e3820e56aebaa9933c03bb45ad0
Unknown <guilgaly@gmail.com>
2018-11-03 21:36:45 +0100

    Add default mode

--- core/src/cowsay4s/core/CowMode.scala
+++ core/src/cowsay4s/core/CowMode.scala
@@ -4,19 +4,21 @@ import scala.collection.immutable
 
 import enumeratum.{Enum, EnumEntry}
 
-sealed abstract class CowMode(val code: String, val face: CowFace)
+sealed abstract class CowMode(val face: CowFace)
     extends EnumEntry
 
 object CowMode extends Enum[CowMode] {
 
-  case object Borg extends CowMode("b", CowFace("==", "  "))
-  case object Dead extends CowMode("d", CowFace("xx", "U "))
-  case object Greedy extends CowMode("g", CowFace("$$", "  "))
-  case object Paranoia extends CowMode("p", CowFace("@@", "  "))
-  case object Stoned extends CowMode("s", CowFace("**", "U "))
-  case object Tired extends CowMode("t", CowFace("--", "  "))
-  case object Wired extends CowMode("w", CowFace("OO", "  "))
-  case object Youthful extends CowMode("y", CowFace("..", "  "))
+  case object Default extends CowMode(CowFace("oo", "  "))
+
+  case object Borg extends CowMode(CowFace("==", "  ")) // b
+  case object Dead extends CowMode(CowFace("xx", "U ")) // d
+  case object Greedy extends CowMode(CowFace("$$", "  ")) // g
+  case object Paranoia extends CowMode(CowFace("@@", "  ")) // p
+  case object Stoned extends CowMode(CowFace("**", "U ")) // s
+  case object Tired extends CowMode(CowFace("--", "  ")) // t
+  case object Wired extends CowMode(CowFace("OO", "  ")) // w
+  case object Youthful extends CowMode(CowFace("..", "  ")) // y
 
   override def values: immutable.IndexedSeq[CowMode] = findValues
 }
