2772a592b33fc4788b408e557c9e385ad2f7d7c5
Guillaume Galy <guilgaly@gmail.com>
2020-04-11 15:42:32 +0200

    Update CircleCI build to test on both JDK 8 and JDK 11

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -2,14 +2,21 @@
 
 set -e
 
-echo "[INFO] Run tests (Core)"
-
 scala_version="$1"
 echo "Scala version: $scala_version"
-
 additional_build="$2"
 echo "Additional build: $additional_build"
 
+echo "[INFO] Check the source format"
+mill __.reformat
+git diff --exit-code || (cat >> /dev/stderr <<EOF
+[ERROR] Scalafmt check failed, see differences above.
+To fix, format your sources using 'mill __.reformat'.
+EOF
+false
+)
+
+echo "[INFO] Run tests (Core)"
 mill "_._[$scala_version].test"
 
 if [[ "$additional_build" == "cli" ]]; then
